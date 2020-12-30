fe3782752f0f27054d63ad4e1a7f31f398bccec1
drisstafouzert <driss.tafouzert@gmail.com>
2020-12-30 15:53:33 +0000

    Add init test for AuthService

--- src/app/services/authentification/auth.service.spec.ts
+++ src/app/services/authentification/auth.service.spec.ts
@@ -1,13 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
-
+  let httpMock:HttpTestingController
+  let http:HttpClient
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[AuthService]
+    });
     service = TestBed.inject(AuthService);
+    httpMock=TestBed.get(HttpTestingController)
+    http=TestBed.inject(HttpClient)
   });
 
   it('should be created', () => {
